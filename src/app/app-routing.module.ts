66933f50943eb84944182263c68a921827161750
ImHeniir <henriqueantana2009@hotmail.com>
2023-04-15 01:30:10 -0300

    Pagina de contatos finalizada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.componen
 const routes: Routes = [{ path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
                         { path:"", redirectTo: "produtos", pathMatch:"full"},
                         { path: 'carrinho', loadChildren: () => import('./carrinho/carrinho.module').then(m => m.CarrinhoModule) },
+                        { path: 'contato', loadChildren: () => import('./contato/contato.module').then(m => m.ContatoModule) },
                         {path:"**", component: NaoEncontradaComponent}
 ];
 
