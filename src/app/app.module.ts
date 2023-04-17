e422cb236d8cffdf78ce005d5131a597bdda1cab
ImHeniir <henriqueantana2009@hotmail.com>
2023-04-17 19:54:53 -0300

    Barra de pesquisa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,22 @@ import { FooterComponent } from './footer/footer.component';
 import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { BarraPesquisaComponent } from './barra-pesquisa/barra-pesquisa.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NaoEncontradaComponent
+    NaoEncontradaComponent,
+    BarraPesquisaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {IProduto} from "../produtos";
 import { ProdutosService } from '../produtos.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.component.html',
@@ -10,11 +11,21 @@ export class ProdutosComponent implements OnInit{
   produtos: IProduto[] | undefined;
 
   constructor(
-    private produtosService: ProdutosService
+    private produtosService: ProdutosService,
+    private route: ActivatedRoute
   ){}
 
 ngOnInit(): void{
-  this.produtos = this.produtosService.getAll();
+  const produtos = this.produtosService.getAll();
+  this.route.queryParamMap.subscribe(params => {
+    const descricao = params.get("descricao")?.toLowerCase();
+
+    if(descricao){
+      this.produtos = produtos.filter(produto => produto.descricao.toLowerCase().includes(descricao));
+      return;
+    }
+    this.produtos = produtos;
+  })
 }
 
 }
